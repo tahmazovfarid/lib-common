27d2fe7df62768cd84fcbd46f09641c7fd2a2dec
Farid <farid.tahmazov@ailab.az>
2025-04-02 16:15:41 +0400

    feat: added allowedSortFields to PaginationRequest

--- src/main/java/az/ailab/lib/common/dto/request/PaginationRequest.java
+++ src/main/java/az/ailab/lib/common/dto/request/PaginationRequest.java
@@ -3,40 +3,19 @@ package az.ailab.lib.common.dto.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Pattern;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 public record PaginationRequest(
-        @Min(value = 0, message = "Page index must not be less than zero")
+        @Min(value = 0, message = "${validation.page.min}")
         Integer page,
 
-        @Min(value = 1, message = "Page size must not be less than one")
-        @Max(value = 100, message = "Page size must not be greater than 100")
+        @Min(value = 1, message = "${validation.size.min}")
+        @Max(value = 100, message = "${validation.size.max}")
         Integer size,
 
-        @Pattern(regexp = "^[a-zA-Z]+$", message = "Sort by only contain letters.")
+        @Pattern(regexp = "^[a-zA-Z]+$", message = "${validation.sort.pattern}")
         String sortBy,
 
-        @Pattern(regexp = "^(asc|desc)$", flags = Pattern.Flag.CASE_INSENSITIVE, message = "Direction must be either 'asc' or 'desc'")
+        @Pattern(regexp = "^(asc|desc)$", flags = Pattern.Flag.CASE_INSENSITIVE, message = "${validation.direction.pattern}")
         String direction) {
-
-    public Pageable toPageable() {
-        final String effectiveSortField = sortBy == null || sortBy.isBlank()
-                ? "id"
-                : sortBy.trim();
-
-        final Sort.Direction effectiveDirection = parseDirection(direction);
-
-        return PageRequest.of(page, size, Sort.by(effectiveDirection, effectiveSortField));
-    }
-
-    private Sort.Direction parseDirection(String dir) {
-        if (dir == null || dir.isBlank() || dir.equalsIgnoreCase("asc")) {
-            return Sort.Direction.ASC;
-        }
-        return Sort.Direction.DESC;
-    }
-
 
 }
